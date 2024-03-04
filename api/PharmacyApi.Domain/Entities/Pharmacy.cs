1d412f74439b96cd58b882500b375aedfa547578
Leo Yong <ryong1@uco.edu>
2024-03-04 13:30:20 -0600

    Remove default 0 for fill prescriptions

--- api/PharmacyApi.Domain/Entities/Pharmacy.cs
+++ api/PharmacyApi.Domain/Entities/Pharmacy.cs
@@ -16,24 +16,24 @@ public class Pharmacy
     [Required(ErrorMessage = "The address of the pharmacy is required.")]
     [MaxLength]
     public string Address { get; set; } = null!;
-    
+
     [Required(ErrorMessage = "The city of the pharmacy is required.")]
     [StringLength(50, ErrorMessage = "The city of the pharmacy cannot be longer than 50 characters.")]
     public string City { get; set; } = null!;
-    
+
     [Required(ErrorMessage = "The state of the pharmacy is required.")]
     [StringLength(2, ErrorMessage = "The state of the pharmacy cannot be longer than 2 characters.")]
     public string State { get; set; } = null!;
-    
+
     [Required(ErrorMessage = "The zip of the pharmacy is required.")]
     [StringLength(10, ErrorMessage = "The zip of the pharmacy cannot be longer than 10 characters.")]
     public string Zip { get; set; } = null!;
-    
-    public int NumberOfFilledPrescriptions { get; set; } = 0;
-    
+
+    public int NumberOfFilledPrescriptions { get; set; }
+
     [DataType(DataType.DateTime)]
     public DateTime CreatedDate { get; set; }
-    
+
     [DataType(DataType.DateTime)]
     public DateTime UpdatedDate { get; set; }
 }
