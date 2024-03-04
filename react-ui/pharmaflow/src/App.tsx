ad92873954b5c7991d20ac62fe65b51c9ac9c25b
Leo Yong <ryong1@uco.edu>
2024-03-04 13:37:23 -0600

    Removed unneeded styles, default test file, and setting up start of app through App.tsx

--- react-ui/pharmaflow/src/App.tsx
+++ react-ui/pharmaflow/src/App.tsx
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { Outlet, useLocation } from 'react-router-dom';
 import './App.css';
+import PharmacyTable from './components/pharmacies/PharmacyTable';
+import { Container } from 'semantic-ui-react';
+import { useEffect, useState } from 'react';
+import { setupErrorHandlingInterceptor } from './interceptors/axiosInterceptor';
+import { SearchBar } from './components/utilities/SearchBar';
 
 function App() {
+  const location = useLocation();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    setupErrorHandlingInterceptor();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container className='main-style'>
+        <SearchBar onSearch={(term => setSearchTerm(term))} />
+        {location.pathname === '/' ? (
+          <PharmacyTable searchTerm={searchTerm} />) : (
+          <Container>
+            <Outlet />
+          </Container>
+        )}
+      </Container>
     </div>
   );
 }
